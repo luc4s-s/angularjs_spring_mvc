70831f4d89383a6bae3ed869cf6b562167f02255
lucas gomes silava <lucasgomessilva713@gmail.com>
2022-09-14 17:46:43 -0300

    Projeto loja de Livro concluido

---
+++ angularjs_spring_mvc/WebContent/resources/js/app.js
@@ -0,0 +1,310 @@
+var permissao = false;
+// configuraï¿½ï¿½o do mï¿½dulo
+var app = angular.module('loja', [ 'ngRoute', 'ngResource', 'ngAnimate' ]);
+
+
+// configurando rotas
+app.config(function($routeProvider) {
+
+			$routeProvider.when("/clientelist", {
+				controller : "clienteController",
+				templateUrl : "cliente/list.html"
+			})// listar
+			
+			.when("/clienteedit/:id", {
+				controller : "clienteController",
+				templateUrl : "cliente/cadastro.html"
+			})// editar
+			
+			.when("/cliente/cadastro", {
+				controller : "clienteController",
+				templateUrl : "cliente/cadastro.html"
+			})// novo
+			
+			//-------------Fornecedor--------------
+			$routeProvider.when("/fornecedorlist", {
+				controller : "fornecedorController",
+				templateUrl : "fornecedor/list.html"
+			})// listar
+			
+			.when("/fornecedoredit/:id", {
+				controller : "fornecedorController",
+				templateUrl : "fornecedor/cadastro.html"
+			})// editar
+			
+			.when("/fornecedor/cadastro", {
+				controller : "fornecedorController",
+				templateUrl : "fornecedor/cadastro.html"
+			})// novo
+			
+			
+			//--------------Livro---------------------
+				$routeProvider.when("/livrolist", {
+				controller : "livroController",
+				templateUrl : "livro/list.html"
+			})// listar
+			
+			.when("/livroedit/:id", {
+				controller : "livroController",
+				templateUrl : "livro/cadastro.html"
+			})// editar
+			
+			.when("/livro/cadastro", {
+				controller : "livroController",
+				templateUrl : "livro/cadastro.html"
+			})// novo
+			
+			//----------------LOJA---------------
+			.when("/loja/online", {
+				controller : "lojaController",
+				templateUrl : "loja/online.html"
+			})
+			.when("/loja/intensLoja/:itens", {
+				controller : "lojaController",
+				templateUrl : "loja/intensLoja.html"
+			})
+			
+			.when("/loja/pedidoconfirmado/:codigoPedido", {
+				controller : "lojaController",
+				templateUrl : "loja/pedidoconfirmado.html"
+			})
+			
+			.when("/loja/pedidos", {
+				controller : "lojaController",
+				templateUrl : "loja/pedidos.html"
+			})
+			
+			.when("/grafico/media_pedido", {//rota
+				controller : "lojaController",//chama esse controle
+				templateUrl : "grafico/media_pedido.html" //direciona para essa pagina 
+			})
+			
+			.otherwise({
+				redirectTo : "/"
+			});
+			
+			
+});
+
+
+
+
+
+// configuraçoes do controller de clientes -----------------------------------------------
+app.controller('clienteController', function($scope, $http, $location, $routeParams) {
+	
+	if ($routeParams.id != null && $routeParams.id != undefined
+			&& $routeParams.id != ''){// se estiver editando o cliente
+		// entra pra editar
+		$http.get("cliente/buscarcliente/" + $routeParams.id).success(function(response) {
+			$scope.cliente = response;
+			
+			document.getElementById("imagemCliente").src = $scope.cliente.foto;//carrega a imagen quando for editar
+			
+			//------------------ carrega estados e cidades do cliente em edicao
+			setTimeout(function () {
+				$("#selectEstados").prop('selectedIndex', (new Number($scope.cliente.estados.id) + 1));
+				
+				$http.get("cidades/listar/" + $scope.cliente.estados.id).success(function(response) {
+					$scope.cidades = response;
+					setTimeout(function () {
+						$("#selectCidades").prop('selectedIndex', buscarKeyJson(response, 'id', $scope.cliente.cidades.id));
+					}, 1000);
+					
+				}).error(function(data, status, headers, config) {
+					erro("Error: " + status);
+				});
+			
+			}, 1000);
+			//----------------------
+			
+		}).error(function(data, status, headers, config) {
+			erro("Error: " + status);
+		});
+		
+	}else { // novo cliente
+		$scope.cliente = {};
+	}
+	
+	
+	$scope.editarCliente = function(id) {
+		$location.path('clienteedit/' + id);
+	};
+	
+	
+	// Responsï¿½vel por salvar o cliente ou editar os dados
+	$scope.salvarCliente = function() {
+				$scope.cliente.foto = document.getElementById("imagemCliente").getAttribute("src");
+				
+				$http.post("cliente/salvar", $scope.cliente).success(function(response) {
+					$scope.cliente = {};
+					document.getElementById("imagemCliente").src = '';
+					sucesso("Gravado com sucesso!");
+				}).error(function(response) {
+					erro("Error: " + response);
+				});
+  
+      };
+          
+          
+	// listar todos os clientes
+	$scope.listarClientes = function(numeroPagina) {
+		$scope.numeroPagina = numeroPagina;
+		$http.get("cliente/listar/" + numeroPagina).success(function(response) {//fazendo a requisiçao
+			
+			if (response == null || response == '') {
+				$scope.ocultarNavegacao = true;
+			}else {
+				$scope.ocultarNavegacao = false;
+			}
+			
+			$scope.data = response;//trazendo a pagina de registro
+			
+			//---------Inicio total de pagina ----------
+				$http.get("cliente/totalPagina").success(function(response) {
+					$scope.totalPagina = response;
+				}).error(function(response) {
+					erro("Error: " + response);
+				});
+			//---------Fim total da pagina----------
+			
+		}).error(function(response) {
+			erro("Error: " + response);
+		});
+		
+	};
+	
+	$scope.proximo = function () {												//se o numero da pagina for menor que o totadapagina pode avançar
+		if (new Number($scope.numeroPagina) < new Number($scope.totalPagina)) {// se nao faz nada
+		 $scope.listarClientes(new Number($scope.numeroPagina + 1));
+		} 
+	};
+	
+	$scope.anterior = function () {					//se o numero da pagina for maior que o totadapagina pode retonar
+		if (new Number($scope.numeroPagina) > 1) {//se nao faz nada
+		  $scope.listarClientes(new Number($scope.numeroPagina - 1));
+		}
+	};
+	
+	// remover cliente passado como parametro
+	$scope.removerCliente = function(codCliente) {
+		$http.delete("cliente/deletar/" + codCliente).success(function(response) {//removendo
+			$scope.listarClientes($scope.numeroPagina);//retornando a lista de cliente
+			sucesso("Removido com sucesso!"); 
+		}).error(function(data, status, headers, config) {
+			erro("Error: " + status);
+		});
+	};
+	
+	
+	// carrega as cidades de acordo com o estado passado por parametro
+	$scope.carregarCidades = function(estado) {
+		if (identific_nav() != 'chrome') {// executa se for diferente do chrome
+			$http.get("cidades/listar/" + estado.id).success(function(response) {
+				$scope.cidades = response;
+			}).error(function(data, status, headers, config) {
+				erro("Error: " + status);
+			});
+	  }
+	};
+	
+	// carrega os estados ao iniciar a tela de cadastro 
+	$scope.carregarEstados = function() {
+		$scope.dataEstados = [{}];
+		$http.get("estados/listar").success(function(response) {
+			$scope.dataEstados = response;
+		}).error(function(response) {
+			erro("Error: " + response);
+		});
+	};
+	
+});
+
+
+
+
+
+// mostra msg de sucesso
+function sucesso(msg) {
+    	$.notify({
+        	message: msg
+
+        },{
+            type: 'success',
+            timer: 1000
+        });
+}
+
+// mostra msg de erro
+function erro(msg) {
+	$.notify({
+    	message: msg
+
+    },{
+        type: 'danger',
+        timer: 1000
+    });
+}
+
+// faz a identificaï¿½ï¿½o da posiï¿½ï¿½o correta da cidade do registro para mostrar em ediï¿½ï¿½o
+function buscarKeyJson(obj, key, value)
+{
+    for (var i = 0; i < obj.length; i++) {
+        if (obj[i][key] == value) {
+            return i + 2;
+        }
+    }
+    return null;
+}
+
+// carregar cidades quando ï¿½ navegador chrome usando jQuery
+function carregarCidadesChrome(estado) {
+	if (identific_nav() === 'chrome') {// executa se for chrome
+		$.get("cidades/listarchrome", { idEstado : estado.value}, function(data) {
+			 var json = JSON.parse(data);
+			 html = '<option value="">--Selecione--</option>';
+			 for (var i = 0; i < json.length; i++) {
+				  html += '<option value='+json[i].id+'>'+json[i].nome+'</option>';
+			 }
+			 $('#selectCidades').html(html);
+		});
+  }
+}
+
+// adiciona a imagem ao campo html img
+function visualizarImg() {
+	 var preview = document.querySelectorAll('img').item(1);
+	  var file    = document.querySelector('input[type=file]').files[0];//pegando a imagen que foi adicionada no file
+	  var reader  = new FileReader();
+
+	  reader.onloadend = function () {
+	    preview.src = reader.result;// carrega em base64 a img
+	  };
+
+	  if (file) {//se a imagen for valida 
+	    reader.readAsDataURL(file);	//fica guardada	    
+	  } else {
+	    preview.src = "";//se nao limpa o campo de imagens
+	  }
+	  
+}
+
+// identificar navegador
+function identific_nav(){
+    var nav = navigator.userAgent.toLowerCase();
+    if(nav.indexOf("msie") != -1){
+       return browser = "msie";
+    }else if(nav.indexOf("opera") != -1){
+    	return browser = "opera";
+    }else if(nav.indexOf("mozilla") != -1){
+        if(nav.indexOf("firefox") != -1){
+        	return  browser = "firefox";
+        }else if(nav.indexOf("firefox") != -1){
+        	return browser = "mozilla";
+        }else if(nav.indexOf("chrome") != -1){
+        	return browser = "chrome";
+        }
+    }else{
+    	alert("Navegador desconhecido!");
+    }
+}
